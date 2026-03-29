4f027d64bdb809edf7aa0d6eb99193f114aec9a3
SHISHIR <gaziur35-3051@diu.edu.bd>
2026-03-30 02:50:14 +0600

    Fix local 404 by making basePath conditional

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/krishibangla',
+  basePath: process.env.NODE_ENV === 'production' ? '/krishibangla' : '',
   images: {
     unoptimized: true,
   },
